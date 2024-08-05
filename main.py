05fae129f8f35f65b951fa96e88f1f97853b3718
Debian <mitelux8@gmail.com>
2024-08-05 04:34:51 +0000

    Add: CORS 미들웨어 추가

--- main.py
+++ main.py
@@ -2,8 +2,20 @@ from debug_toolbar.middleware import DebugToolbarMiddleware
 from fastapi import FastAPI
 from api.v1.routes import router as api_router_v1
 from config import settings
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI(debug=settings.DEBUG)
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 # 미들웨어 추가
 app.add_middleware(
     DebugToolbarMiddleware,
